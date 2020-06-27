c982ff8a16fdbdc472c7b54a67592152a9e3f47b
JeremySze <yongyushi@126.com>
2020-06-27 23:09:27 +0800

    第一次提交11111111111111111111111

---
+++ gittest_idea/src/main/java/com/java1995/test.java
@@ -0,0 +1,11 @@
+package com.java1995;
+
+/**
+ * @author Jeremy SZE
+ * @date 2020/6/27 - 23:07
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111111111111111");
+    }
+}
