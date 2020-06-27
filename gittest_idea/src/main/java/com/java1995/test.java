8029cee3a0a68eaaa35c20d9cf3d93cce570c31e
JeremySze <yongyushi@126.com>
2020-06-27 23:23:45 +0800

    第一次提交22222222222222222222222222222

--- gittest_idea/src/main/java/com/java1995/test.java
+++ gittest_idea/src/main/java/com/java1995/test.java
@@ -7,5 +7,6 @@ package com.java1995;
 public class test {
     public static void main(String[] args) {
         System.out.println("11111111111111111111111111111");
+        System.out.println("22222222222222222222222222222");
     }
 }
